c7a0f37335526e2d83ca1f85d6e0c913122859cf
hassan <hassan@nu.edu.pk>
2022-10-30 16:34:14 +0500

    Fixed Table Paramemter

--- step_definitions/steps.js
+++ step_definitions/steps.js
@@ -5,26 +5,12 @@ Given('', () => {
   I.amOnPage('https://demo.reactstorefront.io/');
 });
 
-When('I enter product into search bar', (table) => {
+When('I enter {product} into search bar', (input) => {
+  I.fillField("//header/div[1]/div[1]/div[3]/div[1]/form[1]/div[1]/div[1]/input[1]", input);
+  I.pressKey('Enter');
 
-  for (const record in table.rows) {
-    if (record < 1) {
-      continue;
-    }
-    const cellValue = table.rows[record].cells;
-    const input = cellValue[0].value;
-    I.fillField("//header/div[1]/div[1]/div[3]/div[1]/form[1]/div[1]/div[1]/input[1]", input);
-    I.pressKey('Enter');
-  }
 });
 
-Then('I should see the result in search results', (table) => {
-  for (const record in table.rows) {
-    if (record < 1) {
-      continue;
-    }
-    const cellValue = table.rows[record].cells;
-    const result = cellValue[1].value;
-    I.seeTextEquals(result, '//body[1]/div[1]/main[1]/div[1]/div[2]/div[2]/div[7]/ul[1]/li[1]/div[1]/div[1]/div[1]/div[2]/h6[1]');
-  }
+Then('I should see the {output} in search results', (result) => {
+  I.seeTextEquals(result, '//body[1]/div[1]/main[1]/div[1]/div[2]/div[2]/div[7]/ul[1]/li[1]/div[1]/div[1]/div[1]/div[2]/h6[1]');
 });
